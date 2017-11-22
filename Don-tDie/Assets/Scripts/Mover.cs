001c6ba2f42f0d779f6a090e71fc1097ba89f553
DESKTOP-4NBMNSH\Lucio <lucio.deluca93@gmail.com>
2017-11-22 04:07:54 +0100

    Primo commit del gioco. Aggiunte tutte le principali funzionalita' in modalita' pc. Presenti rimbalzi e movimento del player che insegue il mouse.

---
+++ Don-tDie/Assets/Scripts/Mover.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb;
+	private Ray2D ActualDir;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		difference.Normalize();
+		ActualDir = new Ray2D (transform.position, difference);
+		rb.AddForce (ActualDir.direction * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Player")) {
+			//print ("colpito");
+		}
+		else if(other.gameObject.CompareTag("Background")){
+			//ActualDir.direction.x rappresenta il coseno
+			//ActualDir.direction.y rappresenta il seno
+			rb.velocity =new Vector2(0.0f,0.0f);
+			int quarter = CheckQuarter ();
+			int collider = CheckCollider (other);
+
+			switch (quarter)
+			{
+			case 1:
+				{
+					if (collider == 1) {
+						float angle = Mathf.Asin (ActualDir.direction.y);
+						angle = Mathf.PI / 2 + angle;
+						Vector2 vett = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
+						ActualDir = new Ray2D (transform.position, vett);
+						speed += speed;
+						rb.AddForce (ActualDir.direction * speed);
+						//nuova direzione q2
+					} 
+					else if (collider == 2){
+						float angle = Mathf.Asin (ActualDir.direction.y);
+						angle =  angle- Mathf.PI / 2 ;
+						Vector2 vett = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
+						ActualDir = new Ray2D (transform.position, vett);
+						speed += speed;
+						rb.AddForce (ActualDir.direction * speed);
+						//nuova direzione q4
+					} 
+					else
+						print("errore");//errore
+					break;
+				}
+			case 2:
+				{
+					if(collider==2){
+						float angle = Mathf.Asin (ActualDir.direction.y);
+						angle = angle + Mathf.PI;
+						Vector2 vett = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
+						ActualDir = new Ray2D (transform.position, vett);
+						speed += speed;
+						rb.AddForce (ActualDir.direction * speed);
+						//nuova direzione q3
+					} 
+					else if(collider==3){
+						float angle = Mathf.Acos (ActualDir.direction.x);
+						angle =angle - Mathf.PI/2 ;
+						Vector2 vett = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
+						ActualDir = new Ray2D (transform.position, vett);
+						speed += speed;
+						rb.AddForce (ActualDir.direction * speed);
+						//nuova direzione q1
+					} 
+					else
+						print("errore");//errore
+					break;
+				}
+			case 3:
+				{
+					if(collider==3){
+						float angle = Mathf.Acos (ActualDir.direction.x);
+						angle = angle + Mathf.PI;
+						Vector2 vett = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
+						ActualDir = new Ray2D (transform.position, vett);
+						speed += speed;
+						rb.AddForce (ActualDir.direction * speed);
+						//nuova direzione q4
+					} 
+					else if(collider==4){
+						float angle = Mathf.Asin (ActualDir.direction.y);
+						angle =  angle - Mathf.PI ;
+						Vector2 vett = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
+						ActualDir = new Ray2D (transform.position, vett);
+						speed += speed;
+						rb.AddForce (ActualDir.direction * speed);
+						//nuova direzione q2
+					} 
+					else 
+						print("errore");//errore
+					break;
+				}
+			default://quadrante 4
+				{
+					if(collider==4){
+						float angle = Mathf.Asin (ActualDir.direction.y);
+						angle = Mathf.PI / 2 + angle;
+						Vector2 vett = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
+						ActualDir = new Ray2D (transform.position, vett);
+						speed += speed;
+						rb.AddForce (ActualDir.direction * speed);
+						//nuova direzione q1
+					} 
+					else if(collider==1){
+						float angle = Mathf.Asin (ActualDir.direction.y);
+						angle = angle-Mathf.PI / 2;
+						Vector2 vett = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
+						ActualDir = new Ray2D (transform.position, vett);
+						speed += speed;
+						rb.AddForce (ActualDir.direction * speed);
+						//nuova direzione q3
+					} 
+					else
+						print("errore");//errore
+					break;
+				}
+			}
+		}
+	}
+
+	//Metodo che restituisce il quadrante in cui giace la direzione del proiettile
+	int CheckQuarter(){
+		if (ActualDir.direction.x >= 0)
+			if (ActualDir.direction.y >= 0)
+				return 1;
+			else
+				return 4;
+		else if (ActualDir.direction.y >= 0)
+				return 2;
+			else
+				return 3;
+	}
+
+	//Metodo che restituisce il collider del Background colpito
+	int CheckCollider(Collider2D other){
+		if (other.offset.x > 0)
+			return 1; //collider di destra
+		else if (other.offset.y < 0)
+			return 4; //collider inferiore
+		else if (other.offset.x < 0)
+			return 3; //collider di sinistra
+		else if (other.offset.y > 0)
+			return 2; //collider superiore
+		else
+			return 0; //errore
+	}
+
+}
---
+++ Don-tDie/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private Rigidbody2D rb;
+	private float nextFire;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb.velocity = movement*speed;
+
+		Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		difference.Normalize();
+		float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0f, 0f, rotation_z-90f);
+	}
+
+	void Update(){
+		if (Input.GetButton("Fire1") && Time.time > nextFire) { 
+
+			nextFire = Time.time + fireRate; 
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation); 
+
+		}
+
+	}
+
+}
