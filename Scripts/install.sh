743c36e9d6b3497a0743558b0aa67b8990adf02e
DESKTOP-4NBMNSH\Lucio <lucio.deluca93@gmail.com>
2017-11-27 18:19:01 +0100

    Aggiornamento per Build su travis

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -3,9 +3,9 @@
 # Download Unity3D installer into the container
 #  The below link will need to change depending on the version
 #  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
-echo 'Downloading Unity 5.5.1 pkg:'
-curl --retry 5 -o Unity.pkg https://netstorage.unity3d.com/unity/46dda1414e51/MacEditorInstaller/Unity-2017.2.0f3.pkg?_ga=2.126360077.1030032498.1511796203-986930763.1507331905
-if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
+#echo 'Downloading Unity 5.5.1 pkg:'
+#curl --retry 5 -o Unity.pkg https://netstorage.unity3d.com/unity/46dda1414e51/MacEditorInstaller/Unity-2017.2.0f3.pkg?_ga=2.126360077.1030032498.1511796203-986930763.1507331905
+#if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 # In Unity 5 they split up build platform support into modules which are installed separately
 # By default, only Mac OSX support is included in the original editor package; Windows, Linux, iOS, Android, and others are separate
@@ -15,7 +15,7 @@ curl --retry 5 -o Unity_win.pkg https://netstorage.unity3d.com/unity/46dda1414e5
 if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 # Run installer(s)
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+#echo 'Installing Unity.pkg'
+#sudo installer -dumplog -package Unity.pkg -target /
 echo 'Installing Unity_win.pkg'
 sudo installer -dumplog -package Unity_win.pkg -target /-target /
