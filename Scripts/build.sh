6b7e389fff3a5d1d3d05d8ff16f3b6184160a6f3
DESKTOP-4NBMNSH\Lucio <lucio.deluca93@gmail.com>
2017-11-27 18:20:18 +0100

    Aggiornamento per Travis

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -7,19 +7,19 @@
 
 ## Make the builds
 # Recall from install.sh that a separate module was needed for Windows build support
-echo "Attempting build of ${UNITYCI_PROJECT_NAME} for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildWindowsPlayer "$(pwd)/Build/windows/${UNITYCI_PROJECT_NAME}.exe" \
-	-quit
+#echo "Attempting build of ${UNITYCI_PROJECT_NAME} for Windows"
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#	-batchmode \
+#	-nographics \
+#	-silent-crashes \
+#	-logFile $(pwd)/unity.log \
+#	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+#	-buildWindowsPlayer "$(pwd)/Build/windows/${UNITYCI_PROJECT_NAME}.exe" \
+#	-quit
 
-rc1=$?
-echo "Build logs (Windows)"
-cat $(pwd)/unity.log
+#rc1=$?
+#echo "Build logs (Windows)"
+#cat $(pwd)/unity.log
 
 echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -35,4 +35,5 @@ rc2=$?
 echo "Build logs (OSX)"
 cat $(pwd)/unity.log
 
-exit $(($rc1|$rc2))
+#Remeber to add exit $(($rc1|$rc2))
+exit $(($rc2))
