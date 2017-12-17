3a4ab0dbb2c1a4bf4695e4020d86ba4c355518f9
Francesco Tedesco <tedesco.uni@gmail.com>
2017-12-17 15:41:59 +0100

    Aggiunta nella versione Pc la possibilità di mettere in pausa in gioco premendo "p" sulla tastiera

---
+++ Don-tDie/Assets/Scripts/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+
+    bool paused = false; // il gioco non comincia in pause, se si mette = true per far cominciare il gioco si dovrebbe premere p
+
+    void Update()
+    {
+        if (Input.GetKeyDown("p")) // esegue l'operazione dentro l'if quando viene premuto il pulsante "p" da tastira
+        {
+            paused = !paused; // paused diventa l'opposto
+        }
+        if (paused) // se paused è vero allora il gioco va fermato
+        {
+            Time.timeScale = 0; // così facendo tutto il gioco viene fermato 
+        }
+        else // altrimenti paused è falso, quindi il gioco deve ricominciare
+        {
+            Time.timeScale = 1; // Time ci permette di far ricominciare il gioco
+        }
+    }
+}
