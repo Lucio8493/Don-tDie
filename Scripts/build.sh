d763cf62a61151bf99f3f5bd45e79d6e30247e40
Lucio De Luca <lucio.deluca93@gmail.com>
2017-12-21 18:01:39 +0100

    Quinto tentativo Testing su travis

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,19 +8,17 @@
 ## Run the editor unit tests
 echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-runTests \
 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-testResults $(pwd)/test.xml \
+	-batchmode \
+	-runTests \
 	-testPlatform playmode \
+	-testResults "$(pwd)/${UNITYCI_PROJECT_NAME}"/test.xml \
+	-testFilter "$(pwd)/${UNITYCI_PROJECT_NAME}/Library/ScriptAssemblies/Assembly-CSharp.dll" \
 	-quit
 
 rc0=$?
 echo "Unit test logs"
-cat $(pwd)/test.xml
+cat "$(pwd)/${UNITYCI_PROJECT_NAME}"/test.xml
 # exit if tests failed
 if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
 
